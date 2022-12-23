09a069436bf243ca4a751c72007a3a2c39593c6e
Andrei Lihtgolt <a.lihtgolt@qorporates.com>
2022-12-23 16:16:56 +0200

    to be continue

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,19 +4,6 @@ import styles from "./Footer.module.css";
 
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-function Items({ currentItems }) {
-  return (
-    <>
-      {currentItems &&
-        currentItems.map((item) => (
-          <div>
-            <h3>Item #{item}</h3>
-          </div>
-        ))}
-    </>
-  );
-}
-
 const Footer = ({ itemsPerPage = 3 }) => {
   const { footer } = styles;
   // Here we use item offsets; we could also use page offsets
@@ -37,7 +24,6 @@ const Footer = ({ itemsPerPage = 3 }) => {
   };
   return (
     <footer className={footer}>
-      <Items currentItems={currentItems} />
       <ReactPaginate
         breakLabel="..."
         nextLabel="next >"
--- client/src/ui-kit/PostCard/PostCard.js
+++ client/src/ui-kit/PostCard/PostCard.js
@@ -1,12 +1,12 @@
 import { DislikeIcon, LikeIcon } from "../../assets/icons";
 import styles from "./PostCard.module.css";
 const PostCard = () => {
-  const { post } = styles;
+  const { post, title, image, comment } = styles;
   return (
     <div className={post}>
-      <h4>Title</h4>
-      <img src="" alt="test" />
-      <p>comment</p>
+      <h4 className={title}>Title</h4>
+      <img className={image} src="" alt="test" />
+      <p className={comment}>comment</p>
       <LikeIcon />
       <DislikeIcon />
     </div>
